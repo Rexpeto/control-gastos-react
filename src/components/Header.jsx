902eb8a50ca3ea13e8c433973b33f62bec85f460
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 22:21:00 -0400

    Utilización de state de presupuesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import NuevoPresupuesto from "./NuevoPresupuesto";
 
-const Header = () => {
+const Header = ({presupuesto, setPresupuesto}) => {
     return ( 
         <header>
             <h1>Planificador de gastos</h1>
-            <NuevoPresupuesto />
+            <NuevoPresupuesto 
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
         </header>
     );
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,10 +1,10 @@
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
     return ( 
         <div className="contenedor-presupuesto contenedor sombra">
             <form className="formulario">
                 <div className="campo">
                     <label htmlFor="presupuesto">Definir Presupuesto</label>
-                    <input className="nuevo-presupuesto" type="text" name="presupuesto" id="presupuesto"/>
+                    <input className="nuevo-presupuesto" type="text" name="presupuesto" id="presupuesto" value={presupuesto} onChange={e => setPresupuesto(e.target.value)}/>
                 </div>
                 <input type="submit" value="Añadir" />
             </form>
