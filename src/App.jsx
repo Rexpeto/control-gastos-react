2778a11c228aa1472a6c6511289a35c9a5318ed5
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 23:02:17 -0400

    Iniciando state isValidPresupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Header from "./components/Header";
 
 function App() {
     const [presupuesto, setPresupuesto] = useState(0);
+    const [isValidPresupuesto, setIsvalidPresupuesto] = useState(false);
 
     return (
         <>
             <Header 
                 presupuesto={presupuesto}
                 setPresupuesto={setPresupuesto}
+                setIsvalidPresupuesto={setIsvalidPresupuesto}
+                isValidPresupuesto={isValidPresupuesto}
             />
         </>
     );
