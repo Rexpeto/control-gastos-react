09f9b683211125a8d13cf7f57548f7a6e4b966e4
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 22:20:40 -0400

    State de presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+    const [presupuesto, setPresupuesto] = useState(0);
 
     return (
         <>
-            <Header />
+            <Header 
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
         </>
     );
 }
