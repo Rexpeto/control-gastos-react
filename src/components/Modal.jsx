0f2d2af06548ab604907acd9551642be563646aa
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-29 16:29:43 -0400

    Finalizando formulario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,6 +21,32 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             </div>
             <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
                 <legend>Nuevo Gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input type="text" id='nombre' name='nombre' placeholder='Coloque el nombre del gasto' autoComplete='off'/>
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input type="number" id='cantidad' name='cantidad' placeholder='Coloque la cantidad' autoComplete='off'/>
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Categoria</label>
+                    <select name="categoria" id="categoria">
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+
+                <input type="submit" value="Añadir gasto" />
             </form>
         </div>
     );
