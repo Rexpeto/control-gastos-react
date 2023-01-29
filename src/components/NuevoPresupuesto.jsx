a6f86ce72a3987bf4c5ea562224fc40237e04d79
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 23:00:58 -0400

    Utilizando state isValid

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,31 +1,45 @@
 import { useState } from "react";
 import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-    const [mensaje, setMensaje] = useState('');
+const NuevoPresupuesto = ({
+    presupuesto,
+    setPresupuesto,
+    setIsvalidPresupuesto,
+    isValidPresupuesto,
+}) => {
+    const [mensaje, setMensaje] = useState("");
 
-    const handlePresupuesto = e => {
+    const handlePresupuesto = (e) => {
         e.preventDefault();
 
-        if(!Number(presupuesto) || Number(presupuesto) < 0) {
-            setMensaje('No es un número válido');
-            setPresupuesto(0);
+        if (!presupuesto || presupuesto < 0) {
+            setMensaje("No es un número válido");
+            return;
         }
-    }
+        setMensaje("");
+        setIsvalidPresupuesto(true);
+    };
 
-    return ( 
+    return (
         <div className="contenedor-presupuesto contenedor sombra">
             <form className="formulario" onSubmit={handlePresupuesto}>
                 <div className="campo">
                     <label htmlFor="presupuesto">Definir Presupuesto</label>
-                    <input className="nuevo-presupuesto" type="text" name="presupuesto" id="presupuesto" value={presupuesto} onChange={e => setPresupuesto(e.target.value)}/>
+                    <input
+                        className="nuevo-presupuesto"
+                        type="number"
+                        name="presupuesto"
+                        id="presupuesto"
+                        value={presupuesto}
+                        onChange={(e) => setPresupuesto(Number(e.target.value))}
+                    />
                 </div>
                 <input type="submit" value="Añadir" />
 
-                {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
             </form>
         </div>
     );
-}
+};
 
 export default NuevoPresupuesto;
