c91f8103120192a6b806c4126d35225480df38d6
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 22:44:55 -0400

    Validando el presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+    const [mensaje, setMensaje] = useState('');
+
+    const handlePresupuesto = e => {
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0) {
+            setMensaje('No es un número válido');
+            setPresupuesto(0);
+        }
+    }
+
     return ( 
         <div className="contenedor-presupuesto contenedor sombra">
-            <form className="formulario">
+            <form className="formulario" onSubmit={handlePresupuesto}>
                 <div className="campo">
                     <label htmlFor="presupuesto">Definir Presupuesto</label>
                     <input className="nuevo-presupuesto" type="text" name="presupuesto" id="presupuesto" value={presupuesto} onChange={e => setPresupuesto(e.target.value)}/>
                 </div>
                 <input type="submit" value="Añadir" />
+
+                {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
             </form>
         </div>
     );
