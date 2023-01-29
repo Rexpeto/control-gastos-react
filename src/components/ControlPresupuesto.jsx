e607f71c2b31d3857a2475fee7725ba631880fb7
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-29 15:32:22 -0400

    Mostrando disponible y gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -15,6 +15,10 @@ const ControlPresupuesto = ({presupuesto}) => {
 
             <div className="contenido-presupuesto">
                 <p><span>Presupuesto:</span> ${formatearCantidad(presupuesto)}</p>
+
+                <p><span>Disponible:</span> ${formatearCantidad(0)}</p>
+
+                <p><span>Gastado:</span> ${formatearCantidad(0)}</p>
             </div>
         </div>
     );
