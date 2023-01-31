e8caa91a42de184f1d5a790861361d65bb29047f
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-31 19:10:26 -0400

    Pasando gastos a Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import { generarId } from "./helpers";
 import IconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
 function App() {
+    const [gastos, setGastos] = useState([]);
     const [presupuesto, setPresupuesto] = useState(0);
     const [isValidPresupuesto, setIsvalidPresupuesto] = useState(false);
     const [modal, setModal] = useState(false);
     const [animarModal, setAnimarModal] = useState(false);
-    const [gastos, setGastos] = useState([]);
 
     const handleNuevoGasto = () => {
         setModal(true);
@@ -38,6 +38,7 @@ function App() {
     return (
         <div className={modal ? 'fijar' : ''}>
             <Header 
+                gastos={gastos}
                 presupuesto={presupuesto}
                 setPresupuesto={setPresupuesto}
                 setIsvalidPresupuesto={setIsvalidPresupuesto}
