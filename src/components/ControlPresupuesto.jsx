546f990c9e7e31e07a3e217d217523151abc4727
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-31 19:14:24 -0400

    Mostrando lo disponible del gasto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -7,7 +7,10 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
 
     useEffect(() => {
         const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0);
+        const totalDisponible = presupuesto - totalGastado;
+
         setGastado(totalGastado);
+        setDisponible(totalDisponible);
     }, [gastos]);
 
     const formatearCantidad = cantidad => (
