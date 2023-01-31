783fe4d92536491a72b279e77a47e180457d82a4
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-30 21:48:35 -0400

    Generando fecha al objecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
+import ListadoGastos from "./components/ListadoGastos";
 import { generarId } from "./helpers";
 import IconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
@@ -23,6 +24,8 @@ function App() {
     const guardarGasto = gasto => {
         
         gasto.id = generarId();
+        gasto.fecha = Date.now();
+        
         setGastos([...gastos, gasto]);
 
         setAnimarModal(false);
@@ -41,13 +44,20 @@ function App() {
                 isValidPresupuesto={isValidPresupuesto}
             />
             {isValidPresupuesto && (
-                <div className="nuevo-gasto">
-                    <img 
-                        src={IconoNuevoGasto} 
-                        alt="Nuevo gasto"
-                        onClick={handleNuevoGasto}
-                    />
-                </div>
+                <>
+                    <main>
+                        <ListadoGastos 
+                            gastos={gastos}
+                        />
+                    </main>
+                    <div className="nuevo-gasto">
+                        <img 
+                            src={IconoNuevoGasto} 
+                            alt="Nuevo gasto"
+                            onClick={handleNuevoGasto}
+                        />
+                    </div>
+                </>
             )}
 
             {modal && (
