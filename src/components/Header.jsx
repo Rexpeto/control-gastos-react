b68f2fb99775b6bd7760ef93d16706131f6c00e9
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-04 18:26:17 -0400

    Passando set para reiniciar app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import ControlPresupuesto from "./ControlPresupuesto";
 
 const Header = ({
     gastos,
+    setGastos,
     presupuesto,
     setPresupuesto,
     setIsvalidPresupuesto,
@@ -14,7 +15,10 @@ const Header = ({
             {isValidPresupuesto ? (
                 <ControlPresupuesto 
                     presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
                     gastos={gastos}
+                    setGastos={setGastos}
+                    setIsvalidPresupuesto={setIsvalidPresupuesto}
                 />
             ) : (
                 <NuevoPresupuesto
