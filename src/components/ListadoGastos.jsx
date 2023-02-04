6c61cca948c79680fea0af3ae00700b160bd9e8c
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-02-04 18:05:39 -0400

    Iterando gastos filtrado y mostrando mensaje

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,18 +1,37 @@
 import Gasto from "./Gasto";
 
-const ListadoGastos = ({gastos, setGastoEditar, eliminarGasto}) => {
+const ListadoGastos = ({gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados}) => {
     return ( 
         <div className="listado-gastos contenedor">
-            <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+            {
+                filtro ? (
+                    <>
+                        <h2>{gastosFiltrados.length ? 'Gastos' : `No hay gastos en la categoría ${filtro}`}</h2>
+                        {gastosFiltrados.map(gastos => (
+                            <Gasto
+                                key={gastos.id}
+                                gasto={gastos}
+                                setGastoEditar={setGastoEditar}
+                                eliminarGasto={eliminarGasto}
+                            />
+                        ))}
 
-            {gastos.map(gasto => (
-                <Gasto 
-                    key={gasto.id}
-                    gasto={gasto}
-                    setGastoEditar={setGastoEditar}
-                    eliminarGasto={eliminarGasto}
-                />
-            ))}
+                    </>
+                ) : (
+                    <>
+                        <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+                        {gastos.map(gasto => (
+                            <Gasto 
+                                key={gasto.id}
+                                gasto={gasto}
+                                setGastoEditar={setGastoEditar}
+                                eliminarGasto={eliminarGasto}
+                            />
+                        ))}
+                    </>
+                )
+                
+            }
         </div>
     );
 }
